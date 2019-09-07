ca00bb8ca1278fc4948e5190e5c3412ec73f3afc
Amit Kulkarni <amit3992@gatech.edu>
2019-09-06 23:38:35 -0700

    added logs

--- src/main/java/com/amit3992/dsa/model/Greeter.java
+++ src/main/java/com/amit3992/dsa/model/Greeter.java
@@ -1,7 +1,15 @@
 package com.amit3992.dsa.model;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Greeter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Greeter.class);
     public String sayHello() {
+        LOGGER.info("testing info log");
+        LOGGER.error("testing error log");
+        LOGGER.info("logs are working!");
         return "Hello world!";
     }
 }
